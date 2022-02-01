b8ca3cecf7c73e2b9d33b220f30abdd3176a5158
haydena23 <haydena23@up.edu>
2022-01-31 16:04:45 -0800

    Move scripts to JS script

---
+++ hw3/public/javascripts/scripts.js
@@ -0,0 +1,30 @@
+eventButton = function(event) {
+    var text=$('textarea#notes').val();
+                    var comp="vegan"
+                    if(text.indexOf(comp) != -1) {
+                        alert("Warning: Cheesecake contains dairy!")
+                    }
+                    else {
+                        var num = $("#quantity").val();
+                        var top = $("input[name=flavor]:checked").val().toLowerCase();
+                        var details = $("#notes").val();
+                        $("#form1").empty();
+                        $("#form1").text("Thank you for your order! You ordered " + num + " " + top + " cheesecakes. The notes for your order are: " + details);
+                    }
+}
+
+eventDropdown = function(event) {
+    var $dropdown = $(dropdown);
+    $dropdown.find('.dropdown-content a').on('click', function() {
+        $dropdown.find("#dropbutton").text($(this).text()).append('');
+    });
+}
+
+$(document).ready(function() {
+    $(function() {
+        $("button").click(eventButton);
+    });
+    $(function() {
+        $('dropdown').each()
+    });
+});
